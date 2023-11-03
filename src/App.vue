a90dc42786887485515f4652959ee378ad4f868c
TOM-TITO <126035168+Tito-com@users.noreply.github.com>
2023-11-03 21:03:11 +0700

    second

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>This is my first vue project !!</h1>
+</template>
+
+<script>
+export default{
+  name: 'App',
+  data(){
+    return{
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
